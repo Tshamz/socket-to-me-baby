d5a37abfa5f1e875039e68db4d4d012ecd860df9
Tshamz <tylershambora@Tshamz-MacBook-Pro.local>
2015-08-29 20:45:08 -0700

    something new

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-var express     = require('express');
-var app         = express();
-var server      = require('http').Server(app);
-var io          = require('socket.io')(server);
+// var express     = require('express');
+// var app         = express();
+// var server      = require('http').Server(app);
+// var io          = require('socket.io').listen(server);
 
-app.set('port', (process.env.PORT || 5000));
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
+
 app.use(express.static(__dirname + '/public'));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var server = http.createServer(app);
+server.listen(port);
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  });
+  }, 1000);
+
+  console.log("websocket connection open");
+
+  ws.on("close", function() {
+    console.log("websocket connection close");
+    clearInterval(id);
+  });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.set('port', (process.env.PORT || 3000));
+
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(app.router);
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
 // app.use(express.static('public'));
 // app.get('/', function (req, res) {
